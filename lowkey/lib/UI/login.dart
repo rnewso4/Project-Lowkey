b8597e974ed93d8f73ae99ca61402ed8bf196861
rnewso4 <rnewso4@lsu.edu>
2021-10-06 10:58:49 -0500

    login page 1st design done

---
+++ lowkey/lib/UI/login.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.center,
+                colors: <Color>[
+                  Colors.white,
+                  Color(0xffE66B63)
+                ],
+            )
+            )
+          ),
+          const SizedBox(
+            height: 350,
+            width: 400,
+            child: Center(
+              child: Image(
+                height: 250,
+                image: AssetImage('lib/UI/assets/logo.png')),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color(0xffF4F4F4),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(60.0),
+                  topLeft: Radius.circular(60.0)),
+              ),
+              height: 500,
+              width: 400,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.only(
+                      top: 40
+                    ),
+                    child: const Text(
+                      'LOWKEY',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontFamily: 'arial',
+                      )
+                      ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(
+                      top: 10
+                    ),
+                    child: const Text(
+                      'Please login to continue',
+                      style: TextStyle(
+                        color: Color(0xff848484)
+                      )
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 300,
+                    margin: const EdgeInsets.only(top: 50),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                        labelText: 'Email',
+                        hintText: 'tigers@lsu.edu'
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 300,
+                    margin: const EdgeInsets.only(top: 30),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                        labelText: 'Password',
+                      ),
+                      obscureText: true,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  Container(
+                    alignment: const Alignment(1.0, 0.0),
+                    margin: const EdgeInsets.only(top: 20),
+                    padding: const EdgeInsets.only(right: 45),
+                    child: const Text(
+                      'Forgot your password?',
+                      style: TextStyle(
+                        color: Color(0xffE66B63),
+                        decoration: TextDecoration.underline,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12
+                      )
+                      ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 30, bottom: 30),
+                    height: 40,
+                    width: 150,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: <Color>[
+                          Color(0xffFFBAB5),
+                          Color(0xffE66B63)
+                        ],
+                      ),
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'LOGIN',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                        ),
+                      )
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Text('Don\'t have an account? '),
+                      Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Color(0xffE66B63),
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
--- lowkey/lib/main.dart
+++ lowkey/lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:flutter/material.dart';
+import 'package:lowkey/UI/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,55 +16,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Palette.kToDark
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Login(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Lowkey',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
-  }
+class Palette { 
+  static const MaterialColor kToDark = const MaterialColor( 
+    0xffe55f48, // 0% comes in here, this will be color picked if no shade is selected when defining a Color property which doesn’t require a swatch. 
+    const <int, Color>{ 
+      50: const Color(0xffE66B63),//10% 
+      100: const Color(0xffb74c3a),//20% 
+      200: const Color(0xffa04332),//30% 
+      300: const Color(0xff89392b),//40% 
+      400: const Color(0xff733024),//50% 
+      500: const Color(0xff5c261d),//60% 
+      600: const Color(0xff451c16),//70% 
+      700: const Color(0xff2e130e),//80% 
+      800: const Color(0xff170907),//90% 
+      900: const Color(0xff000000),//100% 
+    }, 
+  ); 
 }
