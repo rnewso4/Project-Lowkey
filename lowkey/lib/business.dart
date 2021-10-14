4011158d713ad8cc6cbf00de9a296642b5ae11a4
Kyle McCraine <kylemccraine@gmail.com>
2021-10-14 14:21:18 -0500

    Adding Specials, Business, and Rview Classes

---
+++ lowkey/lib/business.dart
@@ -0,0 +1,7 @@
+class Business() {
+  String _name = "";
+
+  Business(String temp) {
+    _name = temp
+  }
+}
---
+++ lowkey/lib/special.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class MenuItems {
+  String _name = "";
+
+  MenuItems(String s) {
+    _name = s;
+  }
+}
