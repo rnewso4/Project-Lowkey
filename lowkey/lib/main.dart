0463f4b76cb273149d170d90f16e352e91954375
Daniel-E-Wise <roaringxtigers@gmail.com>
2021-10-21 13:32:10 -0500

    flutterfire

--- lowkey/lib/main.dart
+++ lowkey/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(const MyApp());
