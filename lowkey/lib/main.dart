fd48cf7f658b2921d353b04e14499fef22cc185d
Daniel-E-Wise <roaringxtigers@gmail.com>
2021-10-21 11:33:10 -0500

    Flutterfire connection

--- lowkey/lib/main.dart
+++ lowkey/lib/main.dart
@@ -1,7 +1,5 @@
-// ignore_for_file: unnecessary_const
-
 import 'package:flutter/material.dart';
-import 'package:lowkey/UI/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,34 +7,62 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  //final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Palette.kToDark
+        primarySwatch: Colors.blue,
       ),
-      home: const Login(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class Palette { 
-  static const MaterialColor kToDark = const MaterialColor( 
-    0xffe55f48, // 0% comes in here, this will be color picked if no shade is selected when defining a Color property which doesn’t require a swatch. 
-    const <int, Color>{ 
-      50: const Color(0xffE66B63),//10% 
-      100: const Color(0xffb74c3a),//20% 
-      200: const Color(0xffa04332),//30% 
-      300: const Color(0xff89392b),//40% 
-      400: const Color(0xff733024),//50% 
-      500: const Color(0xff5c261d),//60% 
-      600: const Color(0xff451c16),//70% 
-      700: const Color(0xff2e130e),//80% 
-      800: const Color(0xff170907),//90% 
-      900: const Color(0xff000000),//100% 
-    }, 
-  ); 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Lowkey',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
