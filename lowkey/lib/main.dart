03df59ad0182fc2c95a2db2c7d9803f6366d2fdb
Daniel-E-Wise <roaringxtigers@gmail.com>
2021-11-09 13:22:56 -0600

    updateBusiness method

--- lowkey/lib/main.dart
+++ lowkey/lib/main.dart
@@ -5,13 +5,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:lowkey/UI/login.dart';
 //import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  // final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   // This widget is the root of your application.
 
   @override
