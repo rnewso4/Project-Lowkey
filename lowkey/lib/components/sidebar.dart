2cac2db749e5623d68579f87fb1a5e24443a92dd
rnewso4 <rnewso4@lsu.edu>
2021-11-23 13:54:48 -0600

    Files moved

--- lowkey/lib/components/sidebar.dart
+++ lowkey/lib/components/sidebar.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lowkey/components/logo_style.dart';
-import 'package:lowkey/homepage_list.dart';
-import 'package:lowkey/homepage_swipe.dart';
-import 'package:lowkey/login.dart';
-import 'package:lowkey/search_businesses.dart';
+import 'package:lowkey/UI/homepage_list.dart';
+import 'package:lowkey/UI/homepage_swipe.dart';
+import 'package:lowkey/UI/login.dart';
+import 'package:lowkey/UI/search_businesses.dart';
 import 'global.dart' as global;
 import 'page_transition.dart';
 
