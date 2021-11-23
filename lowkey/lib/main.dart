b132b5fe346ce4c64ae498315ff5e288beb1ee17
Kyle McCraine <kylemccraine@gmail.com>
2021-11-23 14:11:10 -0600

    Made Files look neater in the directory

--- lowkey/lib/main.dart
+++ lowkey/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'package:lowkey/UI/create_new.dart';
 //import 'package:lowkey/components/utilities.dart';
-import 'package:lowkey/UI/login.dart';
+import 'package:lowkey/login.dart';
 //import 'package:lowkey/manager_page.dart';
 import 'package:overlay_support/overlay_support.dart';
 import 'package:firebase_core/firebase_core.dart';
